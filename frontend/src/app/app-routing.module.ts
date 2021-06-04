6b7102c4408c97402c649a1fd430881adcaf7fa2
Abel Mendez <abel.m.villagra@gmail.com>
2021-06-03 22:40:40 -0300

    formulario alumno creado

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { IngresoComponent } from './components/ingreso/ingreso.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',component:HomeComponent},
+  {path:'alumnos',component:IngresoComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
