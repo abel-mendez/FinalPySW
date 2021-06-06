feb44497631c6d6461ea3fa28d8d49952139a649
Abel Mendez <abel.m.villagra@gmail.com>
2021-06-06 14:59:41 -0300

    agregando la lista de alumnos

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -4,12 +4,15 @@ import { HomeComponent } from './components/home/home.component';
 import { IngresoComponent } from './components/ingreso/ingreso.component';
 import { LoginComponent } from './components/login/login.component';
 import { AlumnoComponent } from './components/alumno/alumno.component';
+import { TablaAlumnosComponent } from './components/tabla-alumnos/tabla-alumnos.component';
 
 const routes: Routes = [
   {path:'',component:HomeComponent},
-  {path:'alumnos',component:IngresoComponent},
+  {path:'alumnos',component:TablaAlumnosComponent},
   {path:'login',component:LoginComponent},
   {path:'alumno',component: AlumnoComponent},
+  {path:'alumnos/nuevo',component: IngresoComponent},
+  //{path:'alumnos/:id',component: IngresoComponent},
 ];
 
 @NgModule({
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -9,6 +9,7 @@ import { IngresoComponent } from './components/ingreso/ingreso.component';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
 import { AlumnoComponent } from './components/alumno/alumno.component';
+import { TablaAlumnosComponent } from './components/tabla-alumnos/tabla-alumnos.component';
 
 @NgModule({
   declarations: [
@@ -18,7 +19,8 @@ import { AlumnoComponent } from './components/alumno/alumno.component';
     IngresoComponent,
     HomeComponent,
     LoginComponent,
-    AlumnoComponent
+    AlumnoComponent,
+    TablaAlumnosComponent
   ],
   imports: [
     BrowserModule,
