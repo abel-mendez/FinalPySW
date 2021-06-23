54d8426811a685d2d8f95ec3786384e52bba5a44
farinelli123 <avilaalvaro14@gmail.com>
2021-06-23 13:26:07 -0300

    Agregado método para preguntar si un usuario existe

--- backend/routes/usuario.route.js
+++ backend/routes/usuario.route.js
@@ -7,6 +7,7 @@ const router = express.Router();
 // definiendo rutas
 router.post('/', usuarioCtrl.createUsuario);
 router.post('/login', usuarioCtrl.loginUsuario);
+router.post('/nombre', usuarioCtrl.getUsuario);
 
 //exportacion del modulo de rutas
 module.exports = router;
