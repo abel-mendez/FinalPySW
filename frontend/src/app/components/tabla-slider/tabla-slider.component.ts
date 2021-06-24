311a3945369facdfd69e6e62cd14ff9033b50deb
Luis18A <caballeroluis15@gmail.com>
2021-06-24 19:07:47 -0300

    validaciones

--- frontend/src/app/components/tabla-slider/tabla-slider.component.ts
+++ frontend/src/app/components/tabla-slider/tabla-slider.component.ts
@@ -8,6 +8,7 @@ import { SliderService } from 'src/app/services/home/slider.service';
 import { LoginService } from 'src/app/services/login/login.service';
 import { FacebookService, InitParams, LoginResponse } from 'ngx-fb';
 import { ApiMethod } from 'ngx-fb/dist/esm/providers/facebook';
+import { NgForm } from '@angular/forms';
 @Component({
   selector: 'app-tabla-slider',
   templateUrl: './tabla-slider.component.html',
@@ -23,52 +24,52 @@ export class TablaSliderComponent implements OnInit {
   constructor(private sliderService: SliderService,
     private router: Router,
     private toastr: ToastrService,
-    private planService:PlanService,
-    private loginService:LoginService,
-    private fb: FacebookService){
+    private planService: PlanService,
+    private loginService: LoginService,
+    private fb: FacebookService) {
     this.iniciarFb();
     this.sliders = new Array<Slider>();
     this.cargarSliders();
     this.planes = new Array<Plan>();
     this.cargarPlanes();
-      if(this.loginService.userLoggedIn()==true){
-        if(sessionStorage.getItem("perfil")== "entrenador"){
-  
-        }else{
-          alert("No posee los permisos necesarios")
+    if (this.loginService.userLoggedIn() == true) {
+      if (sessionStorage.getItem("perfil") == "entrenador") {
+
+      } else {
+        alert("No posee los permisos necesarios")
         this.router.navigate(['home']);
-        }
-      }else{
-        alert("Debe Loguearse para continuar")
-        this.router.navigate(['login']);
       }
+    } else {
+      alert("Debe Loguearse para continuar")
+      this.router.navigate(['login']);
+    }
   }
 
   ngOnInit(): void {
   }
-  postFb(){
+  postFb() {
     var apiMethod: ApiMethod = "post";
     this.fb.api('/108446041487303/feed', apiMethod,
-    {
-    "message":this.slider.descripcion,
-    "access_token":"EAAcRRBuZBYRQBAP9oEgSr69tsTavQtYM5GK5X7YqUSZAF2o00X9hJpW9GnhG1C4ZCQeIhfl4m6v6KAhCbsIS8DvHQgQ1pbNho2X7zC1sEmRtSQlwJMYtPLjht3lEC53v02ALg9eDTn2dF6J6uBZBx7wAchP7LbZC0Bsk4Ya3RrbSKY57fmIQp7tGysZC7OYlv6nOliZBaVCBiDDoMK7VIcYfyBQlbHZCw8cZD"
-    });
+      {
+        "message": this.slider.descripcion,
+        "access_token": "EAAcRRBuZBYRQBAP9oEgSr69tsTavQtYM5GK5X7YqUSZAF2o00X9hJpW9GnhG1C4ZCQeIhfl4m6v6KAhCbsIS8DvHQgQ1pbNho2X7zC1sEmRtSQlwJMYtPLjht3lEC53v02ALg9eDTn2dF6J6uBZBx7wAchP7LbZC0Bsk4Ya3RrbSKY57fmIQp7tGysZC7OYlv6nOliZBaVCBiDDoMK7VIcYfyBQlbHZCw8cZD"
+      });
   }
-  iniciarFb(){
+  iniciarFb() {
     let initParams: InitParams = {
-    appId: '1989309057884436',
-    autoLogAppEvents : true,
-    xfbml : true,
-    version : 'v7.0'
+      appId: '1989309057884436',
+      autoLogAppEvents: true,
+      xfbml: true,
+      version: 'v7.0'
     };
     this.fb.init(initParams);
   }
-  seleccionar(slider){
-    this.slider=slider;
+  seleccionar(slider) {
+    this.slider = slider;
   }
 
-  seleccionarPlan(plan){
-    this.plan=plan;
+  seleccionarPlan(plan) {
+    this.plan = plan;
   }
 
   onFileChanged(e) {
@@ -83,10 +84,10 @@ export class TablaSliderComponent implements OnInit {
         this.sliders = new Array<Slider>();
         this.cargarSliders();
         this.postFb();
-        this.slider=new Slider();
+        this.slider = new Slider();
       },
       error => {
-       console.log(error);
+        console.log(error);
       });
   }
 
@@ -100,7 +101,7 @@ export class TablaSliderComponent implements OnInit {
         });
       },
       error => {
-       alert('Error al cargar los Sliders')
+        alert('Error al cargar los Sliders')
       }
     )
   }
@@ -135,17 +136,17 @@ export class TablaSliderComponent implements OnInit {
   }*/
 
   modificarSlider(slider: Slider) {
-   this.sliderService.modificarSlider(slider).subscribe(
-    result=>{
-      this.toastr.info("El Slider se modificó correctamente", "Operación exitosa");
-      this.sliders = new Array<Slider>();
-      this.cargarSliders();
-      this.slider = new Slider();
-    },
-    error=>{
-      console.log(error);
-    }
-   )
+    this.sliderService.modificarSlider(slider).subscribe(
+      result => {
+        this.toastr.info("El Slider se modificó correctamente", "Operación exitosa");
+        this.sliders = new Array<Slider>();
+        this.slider = new Slider();
+        this.cargarSliders();
+      },
+      error => {
+        console.log(error);
+      }
+    )
   }
 
   //Plan
@@ -154,22 +155,22 @@ export class TablaSliderComponent implements OnInit {
       result => {
         this.toastr.success("El Plan se agregó correctamente", "Operación exitosa");
         this.planes = new Array<Plan>();
-        this.cargarPlanes();
         this.plan = new Plan();
+        this.cargarPlanes();
       },
       error => {
         console.log(error);
       });
   }
 
-  cargarPlanes(){
+  cargarPlanes() {
     this.planService.getPlanes().subscribe(
       result => {
         result.forEach(element => {
           var plan = new Plan();
           Object.assign(plan, element);
           this.planes.push(plan);
-        
+
         });
       },
       error => {
@@ -178,21 +179,21 @@ export class TablaSliderComponent implements OnInit {
     )
   }
 
-  modificarPlan(plan:Plan) {
+  modificarPlan(plan: Plan) {
     this.planService.modificarPlan(plan).subscribe(
-     result=>{
-       this.toastr.info("El Plan se modificó correctamente", "Operación exitosa");
-       this.planes = new Array<Plan>();
-       this.cargarPlanes();
-       this.plan = new Plan();
-     },
-     error=>{
-       console.log(error);
-     }
+      result => {
+        this.toastr.info("El Plan se modificó correctamente", "Operación exitosa");
+        this.planes = new Array<Plan>();
+        this.plan = new Plan();
+        this.cargarPlanes();
+      },
+      error => {
+        console.log(error);
+      }
     )
-   }
+  }
 
-  eliminarPlan(plan:Plan) {
+  eliminarPlan(plan: Plan) {
     this.planService.deletePlan(plan).subscribe(
       result => {
         console.log(result);
